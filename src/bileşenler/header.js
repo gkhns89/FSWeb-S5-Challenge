3e12aa2e6878d5fa74767336150a2b88d3ce307f
gkhns89 <gokhansisman@outlook.com>
2023-11-10 00:04:45 +0300

    First task completed!

--- src/bileşenler/header.js
+++ src/bileşenler/header.js
@@ -11,19 +11,39 @@ const Header = (baslik, tarih, yazi) => {
   //    <span class="temp">{ yazi }</span>
   //  </div>
   //
-}
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const h1 = document.createElement("h1");
+  const tempature = document.createElement("span");
+
+  date.classList.add("date");
+  tempature.classList.add("temp");
+  header.classList.add("header");
+
+  date.textContent = tarih;
+  h1.textContent = baslik;
+  tempature.textContent = yazi;
+
+  header.appendChild(date);
+  header.appendChild(h1);
+  header.appendChild(tempature);
+
+  return header;
+};
 
 const headerEkleyici = (secici) => {
+  const headerItem = Header("Teknoloji Zamanı", "11.11.2023", "11°C");
+  const headerContainer = document.querySelector(secici);
+  headerContainer.appendChild(headerItem);
+
   // GÖREV 2
   // ---------------------
   // Tek argümanı olarak bir css seçici alan bu fonksiyonu uygulayın.
   // Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
   // Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
   //
-
-  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper")) 
+  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper"))
   // fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
- 
-}
+};
 
-export { Header, headerEkleyici }
+export { Header, headerEkleyici };
